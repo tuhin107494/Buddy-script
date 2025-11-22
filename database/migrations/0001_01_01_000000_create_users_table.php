c7c691a0c89b5e1d2ab289252d818fd36976f606
tuhin107494 <tuhin107494@gmail.com>
2025-11-22 14:00:08 +0600

    project setup initially

---
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->uuid()->unique();
+            $table->string('username', 50)->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('avatar')->nullable();
+            $table->string('cover_photo')->nullable();
+            $table->text('bio')->nullable();
+            $table->boolean('is_verified')->default(false);
+            $table->timestamp('last_seen')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
